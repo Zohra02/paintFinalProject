52482f48e1ca53547473b09746fb302b1e538b70
Your Name <you@example.com>
2022-10-16 19:23:05 +0600

    done push

---
+++ Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finalprojectpaint
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user, pass;
+            user = textBox1.Text;
+            pass = textBox2.Text;
+            if (user == "rimu" && pass == "191071073")
+            {
+                Form1 f1 = new Form1();
+                f1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please try again wrong password :)");
+            }
+        }
+    }
+}
